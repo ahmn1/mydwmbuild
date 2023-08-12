f54c91c0b6aceaa6101d97ad09e05612d2ca00ee
ahmn1 <aashman.sabharwal@gmail.com>
2023-08-13 00:16:45 +0530

    enabled floating layout binding

--- config.h
+++ config.h
@@ -83,7 +83,7 @@ static const Key keys[] = {
 	// { MODKEY,                       XK_t,      setlayout,      {.v = &layouts[0]} },
 	// { MODKEY,                       XK_f,      setlayout,      {.v = &layouts[1]} },
 	// { MODKEY,                       XK_m,      setlayout,      {.v = &layouts[2]} },
-	// { MODKEY,                       XK_space,  setlayout,      {0} },
+	{ MODKEY|ControlMask,           XK_space,  setlayout,      {0} },
 	{ MODKEY,                       XK_t,      togglefloating, {0} },
 	{ MODKEY,                       XK_f,      togglefullscr,  {0} },
 	{ MODKEY,                       XK_0,      view,           {.ui = ~0 } },
